f1b60ea4f2f661bc12124616a82af7ec11f6c60f
lcfernandez <lcfernandez@cos.ufrj.br>
2023-02-02 17:40:49 -0300

    feat: implement hotels router

---
+++ src/routers/hotels-router.ts
@@ -0,0 +1,12 @@
+import { authenticateTicket, authenticateToken } from "@/middlewares";
+import { Router } from "express";
+import { getHotels, getHotelRooms} from "@/controllers";
+
+const hotelsRouter = Router();
+
+hotelsRouter
+    .all("/*", authenticateToken, authenticateTicket)
+    .get("/", getHotels)
+    .get("/:id", getHotelRooms)
+
+export { hotelsRouter };
