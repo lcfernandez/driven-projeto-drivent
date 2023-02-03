4d1e5ca18a2cff423dee3ff4cc57655996fad092
lcfernandez <lcfernandez@cos.ufrj.br>
2023-02-03 16:58:16 -0300

    feat: validate hotel id

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,8 +1,14 @@
+import hotelsService from "@/services/hotels-service";
+
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 
 export function getHotelRooms(req: Request, res: Response) {
     const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+        return res.sendStatus(httpStatus.NO_CONTENT);
+    }
 
     try {
         const rooms = hotelsService.getHotelRooms(id);
