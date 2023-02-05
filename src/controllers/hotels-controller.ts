701493b157c705b4fd6d37b37fb4e7904a451304
lcfernandez <lcfernandez@cos.ufrj.br>
2023-02-05 18:08:43 -0300

    fix: declare async/await statements in hotels controller

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -3,7 +3,7 @@ import hotelsService from "@/services/hotels-service";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 
-export function getHotelRooms(req: Request, res: Response) {
+export async function getHotelRooms(req: Request, res: Response) {
     const id = Number(req.params.id);
 
     if (isNaN(id)) {
@@ -11,7 +11,7 @@ export function getHotelRooms(req: Request, res: Response) {
     }
 
     try {
-        const rooms = hotelsService.getHotelRooms(id);
+        const rooms = await hotelsService.getHotelRooms(id);
 
         res.status(httpStatus.OK).send(rooms);
     } catch(err) {
@@ -23,9 +23,9 @@ export function getHotelRooms(req: Request, res: Response) {
     }
 }
 
-export function getHotels(req: Request, res: Response) {
+export async function getHotels(req: Request, res: Response) {
     try {
-        const hotels = hotelsService.getHotels();
+        const hotels = await hotelsService.getHotels();
 
         res.status(httpStatus.OK).send(hotels);
     } catch(err) {
