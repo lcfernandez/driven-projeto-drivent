31b0d525fd855cf394784119fda48695a1b4d3fb
lcfernandez <lcfernandez@cos.ufrj.br>
2023-02-10 11:58:41 -0300

    feat: implement bookings schema validation

--- src/routers/bookings-router.ts
+++ src/routers/bookings-router.ts
@@ -1,13 +1,14 @@
-import { authenticateTicket, authenticateToken } from "@/middlewares";
+import { authenticateTicket, authenticateToken, validateBody } from "@/middlewares";
 import { Router } from "express";
 import { getBooking, postBooking, putBooking } from "@/controllers";
+import { createBookingSchema } from "@/schemas/bookings-schema";
 
 const bookingsRouter = Router();
 
 bookingsRouter
   .all("/*", authenticateToken)
   .get("/", getBooking)
-  .post("/", authenticateTicket, postBooking)
+  .post("/", authenticateTicket, validateBody(createBookingSchema), postBooking)
   .put("/:bookingId", putBooking);
 
 export { bookingsRouter };
