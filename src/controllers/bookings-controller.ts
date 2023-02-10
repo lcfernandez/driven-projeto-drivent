0dc920d8c53cd86c52cdacb3268ce167cc69b67b
lcfernandez <lcfernandez@cos.ufrj.br>
2023-02-10 13:27:29 -0300

    feat: implement bookings constroller

---
+++ src/controllers/bookings-controller.ts
@@ -0,0 +1,28 @@
+import { AuthenticatedRequest } from "@/middlewares";
+import { Response } from "express";
+import httpStatus from "http-status";
+
+export async function getBooking(req: AuthenticatedRequest, res: Response) {
+  return;
+}
+
+export async function postBooking(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const { roomId } = req.body;
+
+  try {
+    const booking = await bookingsService.postBooking(userId, roomId);
+
+    res.status(httpStatus.CREATED).send(booking);
+  } catch (err) {
+    if (err.name === "NotFoundError") {
+      return res.sendStatus(httpStatus.NOT_FOUND);
+    }
+
+    res.sendStatus(httpStatus.FORBIDDEN);
+  }
+}
+
+export async function putBooking(req: AuthenticatedRequest, res: Response) {
+  return;
+}
